e8ff3f1755cd8a2b2367c35aca241f1664dcb40e
Kirill020708 <kirill.tim.020708@mail.ru>
2026-02-02 08:01:48 +0300

    Less LMP margin if TT move is capture
    bench 1045405

--- code/searcher.h
+++ code/searcher.h
@@ -660,7 +660,7 @@ struct Worker {
             				   maxEvaluation <= -MATE_SCORE_MAX_PLY ||
             				   isMateScores);
 
-            if (!beingMated && !isRoot && !isPvNode && movesSearched > 3 + depth * depth * 3 && !isMovingSideInCheck && !isMoveInteresting && historyValue < 0) {
+            if (!beingMated && !isRoot && !isPvNode && movesSearched > 3 + depth * depth * (3 - isTTCapture) && !isMovingSideInCheck && !isMoveInteresting && historyValue < 0) {
             	continue;
             }
             
