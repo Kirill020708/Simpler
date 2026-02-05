452e29f346293618fcddee1435effdfa06842756
Kirill020708 <kirill.tim.020708@mail.ru>
2026-02-05 13:20:14 +0300

    Mirrored NNUE net
    bench 852667

--- code/board.h
+++ code/board.h
@@ -572,7 +572,7 @@ struct alignas(64) Board {
 
         int wKingPos = (whitePieces & kings).getFirstBitNumber();
         flippedW = (wKingPos & 7) >= 4;
-        int bKingPos = (whitePieces & kings).getFirstBitNumber();
+        int bKingPos = (blackPieces & kings).getFirstBitNumber();
         flippedB = (bKingPos & 7) >= 4;
 
         if (currentColor == "w")
