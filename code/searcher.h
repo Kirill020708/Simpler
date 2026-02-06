c7d3332f92fbfba0cd912178d14bbe57eb20befe
Kirill020708 <kirill.tim.020708@mail.ru>
2026-02-07 01:17:15 +0300

    More aggressive RFP
    bench 824082

--- code/searcher.h
+++ code/searcher.h
@@ -430,7 +430,7 @@ struct Worker {
             !searchStack[ply].excludeTTmove &&
             !isMateScores) {
 
-            int margin = (50 - improving * 30) * max(depth, 1) * max(depth, 1);
+            int margin = (30 - improving * 15) * max(depth, 1) * max(depth, 1);
 
             if (staticEval >= beta + margin)
                 return (staticEval + beta) / 2;
