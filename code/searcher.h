7e3ab1949a52f26146ffb13add1313409745c464
Kirill020708 <kirill.tim.020708@mail.ru>
2026-03-03 10:33:34 +0300

    Don't save best move in qsearch if node is upper_bound
    bench 1107228

--- code/searcher.h
+++ code/searcher.h
@@ -306,6 +306,10 @@ struct Worker {
             	moveListGenerator.generateMoves(board, historyHelper, color, ply, DO_SORT, ONLY_CAPTURES);
             }
         }
+
+        if (type == UPPER_BOUND)
+        	newTTmove = Move();
+        
         transpositionTable.write(board, currentZobristKey, bestScore, rawStaticEval, 0, type, boardCurrentAge,
                                           newTTmove, ply, ttpv);
         return bestScore;
