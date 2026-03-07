d45c6b534a81fae39aa0cd5e202512a6edfaf204
Kirill020708 <kirill.tim.020708@mail.ru>
2026-03-07 07:46:33 +0300

    History pruning move count
    bench 962114

--- code/searcher.h
+++ code/searcher.h
@@ -687,7 +687,7 @@ struct Worker {
 	            if (!isPvNode &&
 	            	movesSearched > 0 &&
 	            	!isMoveInteresting &&
-	            	historyValue < -200 * depth) {
+	            	historyValue < -(200 - movesSearched * 10) * depth) {
 
 	            	continue;
 	            }
