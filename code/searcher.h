ef557739ba5337d6de4692ad02f9a04ef0ac73b3
Kirill020708 <kirill.tim.020708@mail.ru>
2026-03-09 14:09:34 +0300

    Allow RFP min depth to be 0
    bench 1015838

--- code/searcher.h
+++ code/searcher.h
@@ -422,7 +422,7 @@ struct Worker {
             !searchStack[ply].excludeTTmove &&
             !isMateScores) {
 
-            int margin = (30 - improving * 15) * max(depth, 1) * max(depth, 1);
+            int margin = (30 - improving * 15) * max(depth, 0) * max(depth, 0);
 
             if (staticEval >= beta + margin)
                 return (staticEval + beta) / 2;
