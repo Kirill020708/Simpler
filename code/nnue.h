ecaf50839f9000d138a88a420420adc643c073f4
Kirill020708 <kirill.tim.020708@mail.ru>
2026-04-02 12:09:23 +0300

    New net
    bench 875557

--- code/nnue.h
+++ code/nnue.h
@@ -11,7 +11,7 @@
 
 #define INCBIN_SILENCE_BITCODE_WARNING
 #include "incbin.h"
-INCBIN(NETWORK, "code/pairwise.nnue");
+INCBIN(NETWORK, "code/net.nnue");
 
 const int inputSize = 64 * 12, hl1Size = 1024, hl2Size = 16, hl3Size = 32;
 const int w1BlockSize = 4 * hl2Size;
